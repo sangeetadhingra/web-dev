31e76138417c2e8d5a5eccf48ba730d4b0cd335e
sangeetadhingra <dhingra.s@northeastern.edu>
2022-03-29 00:24:53 -0400

    a7 redux sp22

--- src/components/tuiter /Home-Screen/whats-happening.js
+++ src/components/tuiter /Home-Screen/whats-happening.js
@@ -19,10 +19,10 @@ const WhatsHappening = () => {
       </textarea>
             <hr/>
             <span>
-                            <a href="#"><i className="far fa-image me-3"></i></a>
-                            <a href="#"><i className="far fa-analytics me-3"></i></a>
-                            <a href="#"><i className="far fa-smile me-3"></i></a>
-                            <a href="#"><i className="far fa-calendar me-3"></i></a>
+                            <a href="#top"><i className="far fa-image me-3"></i></a>
+                            <a href="#top"><i className="far fa-analytics me-3"></i></a>
+                            <a href="#top"><i className="far fa-smile me-3"></i></a>
+                            <a href="#top"><i className="far fa-calendar me-3"></i></a>
                         </span>
             <button onClick={tuitClickHandler} className="btn btn-primary fa-pull-right rounded-pill">
                 Tweet
