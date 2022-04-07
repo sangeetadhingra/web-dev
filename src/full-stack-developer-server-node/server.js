daf1dafbab055b72ff6732c1aa955e283dd2e0d0
sangeetadhingra <dhingra.s@northeastern.edu>
2022-04-07 00:00:10 -0400

    am

---
+++ src/full-stack-developer-server-node/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import helloController from "./controllers/hello-controller.js";
+import userController from "./controllers/user-controller.js";
+import tuitsController from "./controllers/tuits-controller.js";
+import cors from 'cors';
+const app = express();
+app.use(cors());
+//app.get('/hello',(req, res) => {res.send('Hello World')})
+app.get('/', (req, res) => {res.send('Welcome to FullStack Development!')})
+helloController(app);
+app.use(express.json());
+userController(app);
+tuitsController(app);
+app.listen(4000);
